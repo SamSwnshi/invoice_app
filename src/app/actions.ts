daaa5600268642246a08431233f613d5df1625c5
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-15 12:50:12 +0530

    update invoice status

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,16 +1,18 @@
 "use server";
 
 import { db } from "@/db";
-import { Invoices } from "@/db/schema";
+import { Invoices, Status } from "@/db/schema";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 export async function createAction(formData: FormData) {
-  const {userId} = await auth();
+  const { userId } = await auth();
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
 
-  if(!userId){
+  if (!userId) {
     return;
   }
   const results = await db
@@ -26,4 +28,23 @@ export async function createAction(formData: FormData) {
     });
 
   redirect(`/invoices/${results[0].id}`);
+}
+
+export async function updateStatusAction(formData: FormData) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return;
+  }
+
+  const id = formData.get("id") as string;
+  const status = formData.get("status") as Status;
+
+  const results = await db
+    .update(Invoices)
+    .set({ status })
+    .where(and(eq(Invoices.id, parseInt(id)), eq(Invoices.userId, userId)))
+    
+  console.log('results',results)
+  revalidatePath(`/invoices/${id}`,'page')
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,9 @@
 import { integer, pgEnum, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {AVAILABLE_STATUSES} from '@/data/invoices'
 
-export const statusEnum = pgEnum('status', ['open', 'paid', 'void', 'uncollectible']);
+export type Status = typeof AVAILABLE_STATUSES[number]["id"];
+const statuses = AVAILABLE_STATUSES.map(({id})=> id) as Array<Status>;
+export const statusEnum = pgEnum('status', statuses as [Status,...Array<Status>]);
 
 export const Invoices = pgTable("invoice", {
   id: serial('id').primaryKey().notNull(),
