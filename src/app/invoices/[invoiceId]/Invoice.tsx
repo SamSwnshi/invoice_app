c9fd3c0d74a78f565d4e5249391bfa15bd9b7e0d
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-15 15:33:38 +0530

    optimizing invoice client side

---
+++ src/app/invoices/[invoiceId]/Invoice.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { Badge } from "@/components/ui/badge";
+
+import { Invoices } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import Container from "@/components/Container";
+import { ChevronDown } from "lucide-react";
+import { useOptimistic } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+import { AVAILABLE_STATUSES } from "@/data/invoices";
+import { updateStatusAction } from "@/app/actions";
+
+interface InvoiceProps {
+  invoice: typeof Invoices.$inferSelect;
+}
+export default function Invoice({ invoice }: InvoiceProps) {
+  const [currentStatus, setCurrentStatus] = useOptimistic(
+    invoice.status,
+    (state, newStatus) => {
+      return String(newStatus);
+    }
+  );
+
+  async function handleOnUpdateStatus(formData: FormData) {
+    const originalStatus = currentStatus;
+    setCurrentStatus(formData.get('status'))
+    try {
+      await updateStatusAction(formData)
+    } catch (error) {
+      setCurrentStatus(originalStatus)
+    }
+  }
+  return (
+    <main className="w-full h-full">
+      <Container>
+        <div className="flex justify-between items-center mb-8  w-full">
+          <h1 className="text-3xl font-semibold  flex items-center gap-6">
+            Invoice {invoice.id}
+            <Badge
+              className={cn(
+                "rounded-full capitalize",
+                currentStatus === "open" && "bg-green-500",
+                currentStatus === "paid" && "bg-green-700",
+                currentStatus === "void" && "bg-zinc-700",
+                currentStatus === "uncollectible" && "bg-red-600"
+              )}
+            >
+              {currentStatus}
+            </Badge>
+          </h1>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="flex items-center gap-2" variant="outline">
+                Change Status <ChevronDown className="w-4 h-auto" />{" "}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {AVAILABLE_STATUSES.map((status) => {
+                return (
+                  <DropdownMenuItem key={status.id}>
+                    <form action={handleOnUpdateStatus}>
+                      <input type="hidden" name="id" value={invoice.id} />
+                      <input type="hidden" name="status" value={status.id} />
+                      <button>{status.label}</button>
+                    </form>
+                  </DropdownMenuItem>
+                );
+              })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <p className="text-3xl mb-3">${invoice.value / 100}</p>
+        <p className="text-lg mb-8">{invoice.description.toUpperCase()}</p>
+        <h2 className="font-bold text-lg mb-4">Billing Details</h2>
+        <ul className="grid gap-2">
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice ID
+            </strong>
+            <span>{invoice.id}</span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Date
+            </strong>
+            <span>{new Date(invoice.createTs).toLocaleDateString()}</span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Name
+            </strong>
+            <span></span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Email
+            </strong>
+            <span></span>
+          </li>
+        </ul>
+      </Container>
+    </main>
+  );
+}
