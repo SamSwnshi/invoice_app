c68016715022eaf8fd10aca9d6e9395a39c5a558
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-28 13:05:21 +0530

    few tweaks

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils"
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 const Container = ({children, className, ...props}: React.ComponentProps<'div'>) => {
     return(
         <div {...props} className={cn('max-w-5xl mx-auto px-5',className)}>
